47fe3834511f984f4238f47c252b4ad574bb88eb
pborges <pborges475@gmail.com>
2015-09-05 08:20:59 -0700

    Add a linux binary and clean flag

--- README.md
+++ README.md
@@ -6,4 +6,31 @@ Im not sure if this will work on every PC
 
 but on my PC I noticed the first 16 bytes of every encrypted file was the same...
 
-to run on windows download the EXE in the dist directory
+pre built binaries included in the dist directory
+
+
+**Usage**
+```
+cryptofinder <start directory> [clean]
+```
+
+**Ex**
+scan all files in the c:\ recursively
+```
+cryptofinder c:\
+```
+
+scan all files in the c:\ recursively and delete all the HELP_DECRYPT files while searching
+```
+cryptofinder c:\ clean
+```
+
+scan all files in the /home/pborges directory recursively
+```
+cryptofinder /home/pborges
+```
+
+scan all files in the /home/pborges directory recursively and delete all the HELP_DECRYPT files while searching
+```
+cryptofinder /home/pborges clean
+```
--- cryptofinder.go
+++ cryptofinder.go
@@ -12,6 +12,7 @@ import (
 var foundBanners []string
 var foundFiles []string
 var infectedHeader []byte
+var clean bool
 
 func visit(path string, f os.FileInfo, err error) error {
 	if !f.IsDir() {
@@ -21,6 +22,10 @@ func visit(path string, f os.FileInfo, err error) error {
 			f, _ := filepath.Abs(path)
 			foundBanners = append(foundBanners, filepath.Dir(f))
 			log.Println("FOUND BANNER:", f)
+			if clean {
+				log.Println("Delete:", f)
+				os.Remove(f)
+			}
 		} else {
 			f, _ := filepath.Abs(path)
 			file, err := os.Open(f)
@@ -55,29 +60,37 @@ func uniq(list []string) []string {
 
 func main() {
 	infectedHeader = []byte{0xF4, 0x26, 0xA9, 0xD9, 0x4A, 0x01, 0x3F, 0x0C, 0x6C, 0x13, 0x04, 0x95, 0xE6, 0x3E, 0x2F, 0x45}
+	infectedFileName := "infected_files.txt"
+	infectedDirectoryFileName := "infected_directories.txt"
 
 	flag.Parse()
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		fmt.Println("usage: cryptofinder <start directory>")
 		return
 	}
+
 	root := flag.Arg(0)
 	log.Println("Starting at", root)
+
+	if len(os.Args) >= 3 && flag.Arg(1) == "clean" {
+		clean = true
+		log.Println("Deleteing banners")
+	}
 
 	err := filepath.Walk(root, visit)
 	if err != nil {
 		log.Fatal("filepath.Walk() returned %v\n", err)
 	}
 
-	os.Remove("infected_directories.txt")
-	dirLog, err := os.OpenFile("infected_directories.txt", os.O_WRONLY | os.O_CREATE, 0755)
+	os.Remove(infectedDirectoryFileName)
+	dirLog, err := os.OpenFile(infectedDirectoryFileName, os.O_WRONLY | os.O_CREATE, 0755)
 	if err != nil {
 		panic(err)
 	}
 	defer dirLog.Close()
 
-	os.Remove("infected_files.txt")
-	fileLog, err := os.OpenFile("infected_files.txt", os.O_WRONLY | os.O_CREATE, 0755)
+	os.Remove(infectedFileName)
+	fileLog, err := os.OpenFile(infectedFileName, os.O_WRONLY | os.O_CREATE, 0755)
 	if err != nil {
 		panic(err)
 	}
